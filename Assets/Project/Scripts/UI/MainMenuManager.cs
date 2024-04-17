3bcc9cb9fd646fe2a824cb1a9b209aa03ab607d9
Racdy <racdy_99@hotmail.com>
2024-04-17 00:56:43 -0600

    Implementación de botones del menú principal
    Se ha agregado la animación a los botones en el menú principal. El menú se encuentra en la escena de MainMenu. Los botones aun no tiene una función en si, solo tiene la animación

---
+++ Assets/Project/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public GameObject mainMenu;
+
+    public void ContinueGame()
+    {
+
+    }
+
+    public void NewGame()
+    {
+
+    }
+
+    public void Options()
+    {
+
+    }
+
+    public void Credits()
+    {
+
+    }
+
+    public void ExitGame()
+    {
+
+    }
+}
